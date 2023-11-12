180fb8eb40d9591e1ac8ee85a209133d78459e2d
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-11-12 14:46:43 +0500

    updated Latest

--- src/Containers/ProductsComponents.jsx
+++ src/Containers/ProductsComponents.jsx
@@ -3,23 +3,31 @@ import { useDispatch, useSelector } from "react-redux";
 
 const ProductsComponents = () => {
   const products = useSelector((state) => state.allProducts.products);
-
-
-  return (
-    <div className="four column wide">
+    
+  const renderList = products.data.map((product)=> {
+     const {id,title,image,price,category} = product;
+        return (
+            <div className="four column wide" key={id}>
       <div className="ui link cards">
         <div className="card">
           <div className="image">
-            {/* Additional content can be added here */}
-          </div>
+            <img src={image} alt={title}/>
+         </div>
           <div className="content">
             <div className="header">
-aaa
+                {title}
             </div>
+            <div className="meta price">$ {price}</div>
+            <div className="meta">{category}</div>
           </div>
         </div>
       </div>
     </div>
+        );
+    })
+
+  return (
+    <>{renderList}</>
   );
 };
 
